76aa46761f05f41b53b12bad1e4036bcbf14549c
Raiden <sagarreddy162002@gmail.com>
2024-11-21 11:58:48 +0530

    Update settings.py

--- QR_code_Attendence/EmployeeAttendance/settings.py
+++ QR_code_Attendence/EmployeeAttendance/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com' # your mail provider like google, yahoo
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'your email.id'
